3db345f7b581e34e873d574cbb4f78de1e3cdf50
PC <PC@MacBook-Pro-5.local>
2026-03-10 23:17:18 -0500

    Switch to HashRouter and fix routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import "./index.css";
-import {BrowserRouter, Routes, Route } from "react-router-dom"
+import {HashRouter, Routes, Route } from "react-router-dom"
 import {Home} from "./pages/Home"
 import {NotFound} from "./pages/NotFound"
 
 function App() {
-  
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route index element={<Home />}/>
           <Route path="*" element={<NotFound />}/>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   )
 }
